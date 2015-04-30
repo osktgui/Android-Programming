a8c916ad76029f38e934483952cbb2f308b4d04a
luisoscategui <luis.oscategui@xurface.com>
2015-04-29 19:42:06 -0500

    Fundaments, Layouts

--- app/src/main/java/com/xurface/geoquiz/QuizActivity.java
+++ app/src/main/java/com/xurface/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.xurface.geoquiz;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -30,10 +31,14 @@ public class QuizActivity extends ActionBarActivity {
 
     private int mCurrentIndex = 0;
 
+    private final static String TAG = "QuizActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Log.d(TAG, "onCreate() called");
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         updateQuestion();
@@ -89,6 +94,36 @@ public class QuizActivity extends ActionBarActivity {
         }
 
         Toast.makeText(this, messageResourceId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
     }
 
 
